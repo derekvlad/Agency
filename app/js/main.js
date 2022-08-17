b7c6223b7211c273fe49341dc1cf9839be6390b1
derekvlad <bravis96166@gmail.com>
2022-08-17 22:42:22 +0300

    adding my files to my repository

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+    var mixer = mixitup('.filter-nav__inner');
+    
+    $('.design-btn, .branding-btn, .illustration-btn , .motion-btn').on('click', function(){
+        $('.filter-nav__inner').addClass('filter-nav__inner--flex');
+    });
+    $('.all-btn').on('click', function(){
+        $('.filter-nav__inner').removeClass('filter-nav__inner--flex');
+    })
+     
+    $('.team__slider-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="images/team/ar-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="images/team/ar-right.svg" alt=""></button>',
+      });
+
+
+});

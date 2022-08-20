c2466b56981a4d21bf2fd1a555e3d311d8f4802d
derekvlad <bravis96166@gmail.com>
2022-08-20 20:25:50 +0300

    add FAQ

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,32 @@
 $(function () {
-    var mixer = mixitup('.filter-nav__inner');
-    
-    $('.design-btn, .branding-btn, .illustration-btn , .motion-btn').on('click', function(){
-        $('.filter-nav__inner').addClass('filter-nav__inner--flex');
+    // var accordion = function (){
+    //     var data = $('.faq__accordion').attr('data-accordion');
+    //     $('.faq__item').on('click', function(){
+    //         if (data === 'close'){
+    //             $('.faq__item-text').slideUp();
+    //             if($(this).hasClass('active')){
+    //                 $(this).toggleClass('active');
+    //             }
+    //             else{
+    //                 $('.faq__item-text').removeClass('active')
+    //                 $(this).toggleClass('active');
+
+    //             }
+    //         }
+    //         else {
+    //             $(this).toggleClass('active');
+               
+    //         }
+    //         // $(this).next('.faq__item-text').not(':animated').slideToggle();
+    //     });
+    // };
+    // accordion();
+
+    $('.faq__item').accordion({
+        "transitionSpeed": 400
     });
-    $('.all-btn').on('click', function(){
-        $('.filter-nav__inner').removeClass('filter-nav__inner--flex');
-    })
-     
+
+
     $('.team__slider-items').slick({
         infinite: true,
         slidesToShow: 4,
@@ -15,7 +34,17 @@ $(function () {
         dots: true,
         prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="images/team/ar-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="images/team/ar-right.svg" alt=""></button>',
-      });
+    });
 
+   
+
+    var mixer = mixitup('.filter-nav__inner');
+
+    $('.design-btn, .branding-btn, .illustration-btn , .motion-btn').on('click', function () {
+        $('.filter-nav__inner').addClass('filter-nav__inner--flex');
+    });
+    $('.all-btn').on('click', function () {
+        $('.filter-nav__inner').removeClass('filter-nav__inner--flex');
+    })
 
 });
